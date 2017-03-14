ba3f796679ba63a595d80d53b5583b384c0b13dc
Trevor Gipper <trevor.gipper@gmail.com>
2017-03-14 15:30:57 -0700

    complete checkpoint 8

---
+++ scripts/landing.js
@@ -0,0 +1,30 @@
+var animatePoints = function () {
+    'use strict';
+    var points = document.getElementsByClassName('point');
+                
+    var revealFirstPoint = function () {
+        points[0].style.opacity = 1;
+        points[0].style.transform = "scaleX(1) translateY(0)";
+        points[0].style.mstransform = "scaleX(1) translateY(0)";
+        points[0].style.WebkitTransform = "scaleX(1) TranslateY(0)";
+    };
+                
+    var revealSecondPoint = function () {
+        points[1].style.opacity = 1;
+        points[1].style.transform = "scaleX(1) translateY(0)";
+        points[1].style.mstransform = "scaleX(1) translateY(0)";
+        points[1].style.WebkitTransform = "scaleX(1) TranslateY(0)";
+    };
+                
+    var revealThirdPoint = function () {
+        points[2].style.opacity = 1;
+        points[2].style.transform = "scaleX(1) translateY(0)";
+        points[2].style.mstransform = "scaleX(1) translateY(0)";
+        points[2].style.WebkitTransform = "scaleX(1) TranslateY(0)";
+    };
+                
+    revealFirstPoint();
+    revealSecondPoint();
+    revealThirdPoint();
+                
+};
