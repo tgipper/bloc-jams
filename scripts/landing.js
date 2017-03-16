a054f17814dfc15ab8c12dcfdf619bc6294a2ba4
Trevor Gipper <trevor.gipper@gmail.com>
2017-03-16 09:53:13 -0700

    Assignment 8, JavaScript console gives reference error: animatePoints is not defined.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,14 @@
 var animatePoints = function () {
     'use strict';
     var points = document.getElementsByClassName('point');
-                
-    var revealFirstPoint = function () {
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.mstransform = "scaleX(1) translateY(0)";
-        points[0].style.WebkitTransform = "scaleX(1) TranslateY(0)";
+    
+    var revealPoint = function(index) {
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0)";
+        points[index].style.mstransform = "scaleX(1) translateY(0)";
+        points[index].style.WebkitTransform = "scaleX(1) TranslateY(0)";
     };
-                
-    var revealSecondPoint = function () {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.mstransform = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) TranslateY(0)";
-    };
-                
-    var revealThirdPoint = function () {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.mstransform = "scaleX(1) translateY(0)";
-        points[2].style.WebkitTransform = "scaleX(1) TranslateY(0)";
-    };
-                
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
-                
+    for (var = i; i<points.length; i++) {
+        revealPoint(i);       
+    }        
 };
