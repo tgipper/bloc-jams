adc40b4f508cbce6628b075a168d5bde1edc6d59
Trevor Gipper <trevor.gipper@gmail.com>
2017-03-16 16:26:06 -0700

    Started assignment 8

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,7 +2,7 @@ var animatePoints = function () {
     'use strict';
     var points = document.getElementsByClassName('point');
     
-    var revealPoint = function(index) {
+    var revealPoint = function (index) {
         points[index].style.opacity = 1;
         points[index].style.transform = "scaleX(1) translateY(0)";
         points[index].style.mstransform = "scaleX(1) translateY(0)";
