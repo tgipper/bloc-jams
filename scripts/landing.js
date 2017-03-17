09469db5ab09c8ff0331746da25cb2a9abe7705f
Trevor Gipper <trevor.gipper@gmail.com>
2017-03-16 23:14:35 -0700

    Initial work on assignment 9.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,29 +1,27 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function (points) {
+var revealPoint = function (point) {
     'use strict';
-    
-    var revealPoint = function (index) {
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.mstransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) TranslateY(0)";
-    };
-    
-    for (var i = 0; i<points.length; i++) {
-        revealPoint(i);
-    }      
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.mstransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) TranslateY(0)";
 };
 
-window.onload = function() {
+var animatePoints = function (points) {
+    'use strict';
+    forEach(points, revealPoint);
+};
+
+window.onload = function () {
     if (window.innerHeight > 950) {
         animatePoints(pointsArray);
     }
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    window.addEventListener('scroll', function(event) {
+    window.addEventListener('scroll', function (event) {
         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
             animatePoints(pointsArray);
         }
     });
-}
+};
