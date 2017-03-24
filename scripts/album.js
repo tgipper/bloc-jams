ee1ccf307fcf91f3999616a4ec7448ffb0559252
Trevor Gipper <trevor.gipper@gmail.com>
2017-03-24 12:47:23 -0700

    Fix play and pause botton errors with centering and mouseover propoerties.

--- scripts/album.js
+++ scripts/album.js
@@ -136,9 +136,8 @@ window.onload = function() {
 	
 	songListContainer.addEventListener('mouseover', function(event) {
 		if (event.target.parentElement.className === 'album-view-song-item') {
-			event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
 			var songItem = getSongItem(event.target);
-
+			
             if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
                 songItem.innerHTML = playButtonTemplate;
             }
